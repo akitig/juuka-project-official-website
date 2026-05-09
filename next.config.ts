26d49d27c89fd10c2782c805efc5858b96dbc332
akitig <akitig24@gmail.com>
2026-05-10 02:57:21 +0900

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const is_dev_deploy = process.env.DEPLOY_TARGET === "dev";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: is_dev_deploy ? "/work/ju-pro" : "",
+  assetPrefix: is_dev_deploy ? "/work/ju-pro" : "",
 };
 
 export default nextConfig;
